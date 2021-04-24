ea4e564c59fa11de28f30393972873d9653e0fb7
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-24 18:29:26 +0530

    ADDED: LinkedHashMap and it's TestCase

--- src/test/java/com/hashtable/TestHashMap.java
+++ src/test/java/com/hashtable/TestHashMap.java
@@ -7,17 +7,39 @@ public class TestHashMap {
     @Test
     public void givenASentenceWhenWordsAreAddedToListShouldReturnWorldFrequency(){
         String sentence = "To be or not to be";
-        HashMap<String, Integer> hashTable = new HashMap<>();
+        HashMap<String, Integer> hashMap = new HashMap<>();
         String[] words = sentence.toLowerCase().split(" ");
         for (String word : words) {
-            Integer value = hashTable.get(word);
+            Integer value = hashMap.get(word);
             if (value == null) value = 1;
             else  value = value + 1;
-            hashTable.add(word, value);
+            hashMap.add(word, value);
         }
-        int frequency = hashTable.get("or");
-        hashTable.linkedList.printNode();
+        int frequency = hashMap.get("or");
+        hashMap.linkedList.printNode();
         System.out.println(frequency);
         Assertions.assertEquals(1,frequency);
+    }
+
+    @Test
+    public void givenASentence_whenWordsAreAddedToList_ShouldReturnParanoidFrequency(){
+        String sentence = "Paranoids are not paranoid because they are paranoid but because " +
+                          "they keep putting themselves deliberately into paranoid avoidable situations";
+
+        HashMap<String , Integer > linkedHashMap = new HashMap<>();
+        String[] sentenceToWords = sentence.toLowerCase().split(" ");
+        for (String word : sentenceToWords) {
+            Integer value = linkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            linkedHashMap.add(word,value);
+        }
+        int frequency = linkedHashMap.get("paranoid");
+
+        linkedHashMap.linkedList.printNode();
+        System.out.println(frequency);
+        Assertions.assertEquals(3,frequency);
     }
 }
